bee566e2a5a28771f47df049c00e2bfda24416b4
Tkhiem <nguyenthienkhiem2912@gmail.com>
2023-11-09 10:50:26 +0700

    Python is here

---
+++ main.py
@@ -0,0 +1,8 @@
+import uvicorn
+from fastapi import FastAPI;
+app=FastAPI()
+@app.get("/")
+def read_root():
+    return {"Hello":"Word"}
+    if __name__=='__main__':
+        uvicorn.run(app,host-"127.0.0.1",port - 8000)
